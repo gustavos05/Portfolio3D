473abd9680e3ad078f993ef9ea129f79a2586fc4
Gustavo <ingsuarezgustavo@gmail.com>
2023-04-19 16:55:34 -0400

    4

--- src/assets/index.js
+++ src/assets/index.js
@@ -26,7 +26,7 @@ import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
 
-import crm from "./crm.jpg";
+import CRM from "./CRM.jpg";
 import jobit from "./jobit.png";
 import Pokedex from "./Pokedex.jpg";
 import Ecommer from "./Ecommer.jpg";
